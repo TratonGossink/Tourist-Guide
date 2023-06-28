bd0180bb9a07e7d9759132a5ec32dfd99e07043d
Traton Gossink <79613749+TratonGossink@users.noreply.github.com>
2023-06-28 12:06:10 -0500

    Initial Commit

---
+++ Tourist Guide/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Tourist Guide
+//
+//  Created by Traton Gossink on 6/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tourist Guide/Tourist_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tourist_GuideApp.swift
+//  Tourist Guide
+//
+//  Created by Traton Gossink on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Tourist_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
